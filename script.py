9437ef9eae3d397d9c851fd45edcbaa2bd9aefe3
SJCoding0130 <89719864+SJCoding0130@users.noreply.github.com>
2025-11-23 22:55:36 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,3 +1,8 @@
 from datetime import datetime
 
-print("Current time:", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+with open("output.txt", "a") as f:   # append mode
+    f.write(current_time + "\n")
+
+print("Written to output.txt:", current_time)

7b49d915073eac76836c2ffd5cd46d7e9470116b
SJCoding0130 <89719864+SJCoding0130@users.noreply.github.com>
2025-11-24 15:06:51 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,16 @@
 from datetime import datetime
 import pytz
+import os
 
 # Malaysia timezone
 malaysia_tz = pytz.timezone("Asia/Kuala_Lumpur")
 malaysia_time = datetime.now(malaysia_tz).strftime("%Y-%m-%d %H:%M:%S %Z")
 
-# Append timestamp to output.txt
-with open("output.txt", "a") as f:
+# Absolute path to output.txt in repo folder
+output_file = os.path.join(os.environ["GITHUB_WORKSPACE"], "output.txt")
+
+# Append timestamp
+with open(output_file, "a") as f:
     f.write(malaysia_time + "\n")
 
 print("Written to output.txt:", malaysia_time)

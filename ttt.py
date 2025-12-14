2bdfc497e854ff4fa920857b2c9111153361142e
SJCoding0130 <89719864+SJCoding0130@users.noreply.github.com>
2025-12-14 23:53:33 +0800

    Update ttt.py

--- ttt.py
+++ ttt.py
@@ -22,7 +22,7 @@ def wait_for_server(url, timeout=15):
 # -----------------------------
 # Paths
 # -----------------------------
-BASE_DIR = Path(__file__).parent.resolve()  # folder containing view.php
+BASE_DIR = D:\a\tester\tester\repo_b\  # folder containing view.php
 print(f"BASE_DIR = {BASE_DIR}")
 JSON_FOLDER = BASE_DIR / "json"
 OUTPUT_BASE = BASE_DIR / "output"
@@ -95,4 +95,5 @@ php_process.terminate()
 php_process.wait()
 print("PHP server terminated.")
 print("All files & languages processed!")
+
 
